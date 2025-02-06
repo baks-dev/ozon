57484c34d29a4d73bd990ca65a6df24a1f2624a0
kingstar <admin@baks.dev>
2025-02-06 10:37:12 +0300

    Baks Development

--- Messenger/OzonTokenHandler.php
+++ Messenger/OzonTokenHandler.php
@@ -4,16 +4,12 @@ declare(strict_types=1);
 
 namespace BaksDev\Ozon\Messenger;
 
-use BaksDev\Ozon\Repository\OzonTokenCurrentEvent\OzonTokenCurrentEventInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler(priority: 0)]
 final class OzonTokenHandler
 {
-    public function __construct(
-        private readonly OzonTokenCurrentEventInterface $currentEvent,
-    ) {
-    }
+    public function __construct() {}
 
     public function __invoke(OzonTokenMessage $message): void
     {
